78fd10aa814d9554b2f2eccc4f1ed4a5f057fff2
Gu Zhiwei <476158043@qq.com>
2024-11-16 17:51:13 +0800

    添加注释

--- Makefile
+++ Makefile
@@ -178,8 +178,8 @@ ifeq ($(LAB),util)
 	UEXTRA += user/xargstest.sh
 endif
 
-fs.img: mkfs/mkfs README $(UEXTRA) $(UPROGS)
-	mkfs/mkfs fs.img README $(UEXTRA) $(UPROGS)
+# fs.img: mkfs/mkfs README $(UEXTRA) $(UPROGS)
+	# mkfs/mkfs fs.img README $(UEXTRA) $(UPROGS)
 
 -include kernel/*.d user/*.d
 
--- kernel/main.c
+++ kernel/main.c
@@ -7,10 +7,9 @@
 volatile static int started = 0;
 
 // start() jumps here in supervisor mode on all CPUs.
-void
-main()
+void main()
 {
-  if(cpuid() == 0){
+  if(cpuid() == 0){//查询TP寄存器，返回运行着当前代码的核心编号，如果是0(第一个核心)，则执行下列初始化
     consoleinit();
     printfinit();
     printf("\n");
@@ -18,7 +17,7 @@ main()
     printf("\n");
     kinit();         // physical page allocator
     kvminit();       // create kernel page table
-    kvminithart();   // turn on paging
+    kvminithart();   // 该函数通过将之前定义的全局变量kernel_pagetable写入到satp寄存器中来开启分页功能
     procinit();      // process table
     trapinit();      // trap vectors
     trapinithart();  // install kernel trap vector
@@ -29,11 +28,10 @@ main()
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
     userinit();      // first user process
-    __sync_synchronize();
+    __sync_synchronize();//告诉编译器在将started赋值为1之前，完成上面的所有初始化，防止编译器优化而扰乱多核代码执行
     started = 1;
-  } else {
-    while(started == 0)
-      ;
+  } else {//除第一个核心之外的将执行下列代码
+    while(started == 0);//执行空循环直到核心0完成初始化
     __sync_synchronize();
     printf("hart %d starting\n", cpuid());
     kvminithart();    // turn on paging

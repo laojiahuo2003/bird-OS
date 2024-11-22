b3c1c2a5f11671c4c89b42f3afd66eb1cd89990f
Gu Zhiwei <476158043@qq.com>
2024-11-22 17:07:42 +0800

    加锁访问proc

--- kernel/proc.c
+++ kernel/proc.c
@@ -694,9 +694,9 @@ procdump(void)
 
 int cps(void)
 {
-  struct proc *p; //定义一个结构体(进程控制块)
+  struct proc *p=proc; //定义一个结构体(进程控制块)
   // stati();	// 中断
-  // acquire(&proc[0].lock); //加锁
+  acquire(&proc->lock); //加锁
   printf("name \t pid \t state \t \t priority \n"); //罗列所有的pid
   for(p = proc; p < &proc[NPROC]; p++)  //NPROC为64
   {
@@ -707,7 +707,7 @@ int cps(void)
     else if(p->state == RUNNABLE) //可运行队列
     printf("%s \t %d \t RUNNABLE \t %d\n", p->name, p->pid, p->priority);
   }
-  // release(&ptable.lock); //释放锁
+  release(&proc->lock); //释放锁
   return 22; //返回22
 }
 void procnum(uint64 *dst) //获取进程数
